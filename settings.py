51a80cc85ede10e50555f6f7e8eb0000cd393341
sahilsable-24 <sahilsable2407@gmail.com>
2026-09-08 22:23:16 +0530

    add settings configuration

---
+++ settings.py
@@ -0,0 +1,7 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    app_name : str = "Workout Tracker"
+    model_config = SettingsConfigDict(env_file=".env")
+
+settings= Settings()
