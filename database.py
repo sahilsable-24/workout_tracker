aed073cdff2ee2414a9ab8cba1bf2fb4908b3373
sahilsable-24 <sahilsable2407@gmail.com>
2026-09-09 13:02:08 +0530

    add user model and initial alembic migration

--- database.py
+++ database.py
@@ -1,4 +1,8 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase
 from settings import settings
+
+class Base(DeclarativeBase):
+    pass
 
 engine = create_engine(settings.database_url)
